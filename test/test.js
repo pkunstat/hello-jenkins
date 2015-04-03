aa9c540f5a9ed83d81bc8841f39a54284423e535
Petr Kunstat <pkunstat@gmail.com>
2015-04-03 09:05:30 -0600

    zmena3

--- test/test.js
+++ test/test.js
@@ -3,10 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello jenkins', function(done) {
-<<<<<<< HEAD
     request(app).get('/').expect('hello jenkins KUK', done);
-=======
-    request(app).get('/').expect('hello jenkins 2', done);
->>>>>>> c0f39f90190f53bfa16313c19c062a5d64243958
   });
 });
